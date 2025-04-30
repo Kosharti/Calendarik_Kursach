f1a67e85bb2c41cdbc6f7bab9b9b1f8e0fcf65be
kosharti <a@mail.ru>
2025-04-30 12:17:19 +0300

    хуета ебаная 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.calendarik"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.calendarik"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
